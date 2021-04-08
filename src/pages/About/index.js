789a650ce8184993272086c81d44eec8dc065cb0
Joel Straley <joelstraley@gmail.com>
2021-04-07 23:00:14 -0400

    fixed About me page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -25,21 +25,21 @@ function About() {
                     <img src={Headshot} alt="Joel Straley Headshot" className="img-fluid rounded headshot"/>
               </Col>
               <Col size="md-9" id="about">
-                    <p>Joel Straley is currently enrolled in the Columbia Engineering Coding Bootcamp to become a Full Stack Web Developer. <div className="new-line"></div>His previous experience is in Operational Logistics. During the COVID-19 pandemic he worked with {/* <a href="https://www.themigrantkitchennyc.com/">The Migrant Kitchen</a>  */}to build a dynamic logistical strategy to deliver 1,000 meals a day, six days a week to NYC residents in all 5 burroughs.
-                    <div className="new-line"></div>Previously, he was a Vehicle Operations Specialist for Uber where he started as a Driver Support Representative. His focus was as a liaison between Uber, their 5 vehicle rental providers and the Taxi and Limousine Commission. This was vital in laying the logistical groundwork to help increase the availability of Wheelchair accessible commercial vehicles in NYC.</p>
-                    <div className="mx-auto">
+                    <p>Joel Straley is currently enrolled in the Columbia Engineering Coding Bootcamp to become a Full Stack Web Developer.<div className="new-line"></div><br></br> His previous experience is in Operational Logistics. During the COVID-19 pandemic he worked with {/* <a href="https://www.themigrantkitchennyc.com/">The Migrant Kitchen</a>  */}to build a dynamic logistical strategy to deliver 1,000 meals a day, six days a week to NYC residents in all 5 burroughs.
+                    <div className="new-line"></div><br></br>Previously, he was a Vehicle Operations Specialist for Uber where he started as a Driver Support Representative. His focus was as a liaison between Uber, their 5 vehicle rental providers and the Taxi and Limousine Commission. This was vital in laying the logistical groundwork to help increase the availability of Wheelchair accessible commercial vehicles in NYC.</p>
+                    <div className="mx-auto personalLinks">
                     <a href="https://www.linkedin.com/in/joel-straley-559aab97/" target="_blank">
-                    <i className="fab fa-linkedin fa-2x"></i></a>
+                    <i className="fab fa-linkedin fa-2x" style={{ margin: 10 }}>  </i></a>
                     <a href="https://twitter.com/joelstraley" target="_blank">
-                    <i className="fab fa-twitter fa-2x"></i></a>
+                    <i className="fab fa-twitter fa-2x" style={{ margin: 10 }}>   </i></a>
                     <a href="https://github.com/Joelstraley" target="_blank">
-                    <i className="fab fa-github fa-2x"></i></a>
+                    <i className="fab fa-github fa-2x" style={{ margin: 10 }}>   </i></a>
                     <a href="mailto:joelstraley@gmail.com" target="_blank">
-                    <i className="far fa-envelope fa-2x"></i></a>
+                    <i className="far fa-envelope fa-2x" style={{ margin: 10 }}>   </i></a>
                     <a href="tel:937-206-4970"> 
-                    <i className="fas fa-phone-square fa-2x"></i></a>
+                    <i className="fas fa-phone-square fa-2x" style={{ margin: 10 }}></i></a>
                     <div className="new-line"></div>
-                    <a href="./assets/JOEL STRALEY RESUME - 2021 - WEB DEV (1).pdf" download>Download PDF Resume</a>
+                    <a href="JOEL STRALEY RESUME - 2021 - WEB DEV (1).pdf" download>Download PDF Resume</a>
                   </div>
               </Col>
           </Row>
